d0c145ed6028fe1b8e5b0805109aa6c50b5315d5
DESKTOP-537LVVM\orizchak <orizchak@gmail.com>
2019-04-21 23:22:36 +0300

    sss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Header from "./components/header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">bright-source</header>
+        <Header />
       </div>
     );
   }
