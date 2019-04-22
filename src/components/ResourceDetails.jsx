5abed5ed8b017da7152e53138273e76fde83a6b9
DESKTOP-537LVVM\orizchak <orizchak@gmail.com>
2019-04-23 00:56:49 +0300

    awd

--- src/components/ResourceDetails.jsx
+++ src/components/ResourceDetails.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./ResourceDetails.css";
+import help from "../content/help-12.svg";
 
 class ResourceDetails extends Component {
   state = {};
@@ -15,18 +16,31 @@ class ResourceDetails extends Component {
           <div className="title">{resource.name}</div>
 
           <div className="details">
-            <div>GENERAL DETAILS</div>
-            <label className="label">NAME</label>
-            <div className="value">{resource.name}</div>
+            <div className="section">
+              <div className="sub-title">
+                GENERAL DETAILS
+                <img src={help} alt="help" />
+              </div>
 
-            <label className="label">DESCRIPTION</label>
-            <div className="value">{resource.description}</div>
+              <label className="label">NAME</label>
+              <div className="value">{resource.name}</div>
 
-            <label className="label">RESOURCE TYPE</label>
-            <div className="value">{resource.resourceType}</div>
+              <label className="label">DESCRIPTION</label>
+              <div className="value">{resource.description}</div>
 
-            <label className="label">PATH</label>
-            <div className="value">{resource.path}</div>
+              <label className="label">RESOURCE TYPE</label>
+              <div className="value">{resource.resourceType}</div>
+
+              <label className="label">PATH</label>
+              <div className="value">{resource.path}</div>
+            </div>
+
+            <div className="section">
+              <div className="sub-title">
+                PERMITED ACTIONS
+                <img src={help} alt="help" />
+              </div>
+            </div>
           </div>
         </div>
       );
